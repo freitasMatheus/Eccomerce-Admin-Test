931c6d8199818e92b16b445c5e27344c0f774856
Matheus de Freitas <matt.freitas01@gmail.com>
2017-10-19 23:23:39 +0200

    Add CRUD for users
    Create Read Update Delete for users

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -2,39 +2,49 @@
 
 namespace Hcode;
 
+class Model {
 
-class Model
-{
 	private $values = [];
 
-	public function __call($name,$args)
+	public function __call($name, $args)
 	{
-		$method = substr($name,0,3);
-		$fieldName = substr($name,3,strlen($name));
+
+		$method = substr($name, 0, 3);
+		$fieldName = substr($name, 3, strlen($name));
+
 		switch ($method)
-			{
-				case 'get':
-						$this->values[$fieldName];
-				break;
-			
-				case 'set':
-					$this->values[$fieldName] = $args[0];
-				break;
-			}
-	}
-
-	public function setData ($data = array())
-	{
-		foreach ($data as $key => $value) 
 		{
-			$this->{"set".$key}($value);
+
+			case "get":
+				return (isset($this->values[$fieldName])) ? $this->values[$fieldName] : NULL;
+			break;
+
+			case "set":
+				$this->values[$fieldName] = $args[0];
+			break;
+
 		}
+
 	}
 
-	public function getValues ()
+	public function setData($data = array())
 	{
-		return $this->values;
+
+		foreach ($data as $key => $value) {
+			
+			$this->{"set".$key}($value);
+
+		}
+
 	}
+
+	public function getValues()
+	{
+
+		return $this->values;
+
+	}
+
 }
 
-?>
+ ?>
